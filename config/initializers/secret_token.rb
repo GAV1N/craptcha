0de3a2a10e2e6011ec20d7b4ddc91bf39b3e83c6
Gavin Todes <gavin.todes@gmail.com>
2011-09-10 14:14:41 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craptcha::Application.config.secret_token = 'a5f71f9458b909dab5defa5cb22564ec60fd2d59f7e0da7fca9965e9a7229094f44b7d7a1429002893ac542bb28514708fa73fc00ddb0ea95a1a31884667f52c'
